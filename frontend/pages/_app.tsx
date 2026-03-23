1d23c840c1c757b670a7dd242282b1145b333a5c
jader <jaugusthsmf@gmail.com>
2026-03-23 15:46:09 -0300

    Rename favicon to icon_NETSide.png and update reference

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { useEffect } from 'react'
 import ToastProvider from '../components/shared/ToastProvider'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const onFocusIn = (e: FocusEvent) => {
+      const t = e.target as HTMLElement | null
+      if (!t) return
+      if (t instanceof HTMLInputElement || t instanceof HTMLTextAreaElement) {
+        try {
+          t.select()
+        } catch (_) {
+          // ignore
+        }
+      }
+    }
+
+    window.addEventListener('focusin', onFocusIn)
+    return () => window.removeEventListener('focusin', onFocusIn)
+  }, [])
+
   return (
     <ToastProvider>
       <Head>
         <title>Omnigem NETSide</title>
-        <link rel="icon" href="/Icon_NETSide.png" />
+        <link rel="icon" href="/icon_NETSide.png" />
       </Head>
       <Component {...pageProps} />
     </ToastProvider>
